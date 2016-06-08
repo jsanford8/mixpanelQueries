641678501b7c863588ab97076138d171bff10944
Justin Sanford <justin.h.sanford@gmail.com>
2016-06-08 12:37:28 -0700

    dockerize

--- index.js
+++ index.js
@@ -1,37 +1,37 @@
-let express        = require('express');
-let bodyParser     = require('body-parser');
-let methodOverride = require('method-override');
-let app            = express();
-let server         = require('http').Server(app);
-let fs             = require('fs');
-let request        = require('request');
-let config         = require('./config');
+var express        = require('express');
+var bodyParser     = require('body-parser');
+var methodOverride = require('method-override');
+var app            = express();
+var server         = require('http').Server(app);
+var fs             = require('fs');
+var request        = require('request');
+var config         = require('./config');
 
 // set up Express
-let pub = __dirname + '/public';
+var pub = __dirname + '/public';
 app.use(express.static(pub));
 app.use(bodyParser.json());
 app.use(methodOverride());
 app.disable('etag');
 
-let routes = {};
+var routes = {};
 
 // run arbitrary script that happens to exist on the server
 routes.runScript = function(req, res) {
   // try to find the file
-  let script = req.params.script;
+  var script = req.params.script;
   fs.readFile('./scripts/' + script + '.js', 'utf8', function(err, data) {
     // bail out on error
     if (err) {
       res.status(400).send('Script not found');
     } else {
       // figure out our dates (default is last week)
-      let params = {};
+      var params = {};
       if (req.query.from_date && req.query.to_date) {
         params.from_date = req.query.from_date;
         params.to_date = req.query.to_date;
       } else {
-        let d = new Date();
+        var d = new Date();
         params.to_date = d.toISOString().slice(0,10);
         d.setDate(d.getDate() - 7);
         params.from_date = d.toISOString().slice(0,10);
